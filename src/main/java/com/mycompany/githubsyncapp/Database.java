1d647a2ba7ac99fc2db6ef1c722050017302131c
Muhammad Asim <muhammadasim4303@gmail.com>
2025-03-03 20:48:35 +0500

    Created a DB Connection

---
+++ src/main/java/com/mycompany/githubsyncapp/Database.java
@@ -0,0 +1,15 @@
+package com.mycompany.githubsyncapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static final String URL = "jdbc:mysql://localhost:3306/githubsync";
+    private static final String USER = "Asim";
+    private static final String PASSWORD = "Simonriley141";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ src/main/java/com/mycompany/githubsyncapp/GitHubSyncApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.githubsyncapp;
+
+/**
+ *
+ * @author Muhammad Asim
+ */
+public class GitHubSyncApp {
+
+    public static void main(String[] args) {
+        
+    }
+}
