d395c11d6d40cc5bea631dda733039cb96c91118
Muhammad Asim <muhammadasim4303@gmail.com>
2025-03-03 20:58:38 +0500

    Forget password OTP Feature

---
+++ src/main/java/com/mycompany/githubsyncapp/OTPService.java
@@ -0,0 +1,41 @@
+package com.mycompany.githubsyncapp;
+
+import java.util.Properties;
+import java.util.Random;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class OTPService {
+    public static int sendOTP(String email) {
+        if (!UserAuth.emailExists(email)) return -1; // Check if email exists in DB
+
+        int otp = new Random().nextInt(900000) + 100000;
+        String from = "your-email@gmail.com";
+        String password = "your-email-password";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            message.setSubject("Password Reset OTP");
+            message.setText("Your OTP is: " + otp);
+            Transport.send(message);
+            return otp;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+}
